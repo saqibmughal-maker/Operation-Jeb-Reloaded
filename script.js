cc2d26b2cd88d8f973b117ceba3234bd7ace2b7f
Saqib Mughal <drarifmughal116@gmail.com>
2026-07-28 03:45:05 -0700

    Add interactive mission progress system

---
+++ script.js
@@ -0,0 +1,11 @@
+function startMission() {
+
+    document.querySelector(".progress").style.width = "50%";
+
+    document.getElementById("progressText").innerHTML =
+    "50% Complete 🚀";
+
+    document.getElementById("missionMessage").innerHTML =
+    "Mission started! Keep building 💻🔥";
+
+}
